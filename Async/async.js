d09138fec699ce32b9d6891544c73e84eba45a1b
sheesoo21 <sheesoo21@gmail.com>
2020-12-31 01:01:49 +0900

    seoul

--- Async/async.js
+++ Async/async.js
@@ -26,7 +26,8 @@ function delay(ms){
 }
 // const example = new Promise((resolve,reject)=>{resolve('method'), reject('method2')}
 async function getApple(){
-    await delay(1000);
+    await delay(2000);
+    
     return '🍎';
 }
 async function getBanana(){
@@ -43,4 +44,20 @@ async function pickFruits(){
 }
 
 pickFruits().then(console.log);
-console.log(pickFruits());
+
+//3. useful APIs 
+function pickAllFruits(){
+    return Promise.all([getApple(), getBanana()]).then(fruits=>
+        fruits.join(' + '));
+} //Promise안에 내장된 all이라는 API는 프로미스들의 배열을 인수로 받는데
+//이렇게 보내진 프로미스들을 동시에 병렬실행한다.
+//.join은 전달받은 인수(배열)을 String 타입으로 묶는 기능을 한다.
+
+pickAllFruits().then(console.log);
+
+function pickOnlyOne(){
+    return Promise.race([getApple(), getBanana()]);
+} //promise안에 내장된 race라는 api는 배열로 전달된 프로미스들 중
+//가장 먼저 결과값을 반환하는 것을 단 하나만 리턴하는 기능을 한다.
+pickOnlyOne().then(console.log);
+
